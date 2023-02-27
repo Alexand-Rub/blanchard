c455b51f6772d44c01d58d8acd319ace17c2d974
Alexandr Rublev <alexandrublev@yandex.ru>
2023-02-27 12:17:22 +0300

    galary card add

--- js/opener.js
+++ js/opener.js
@@ -1,12 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Открытие поиска
     document.querySelectorAll('.search__opener').forEach(function(item) {
         item.addEventListener('click', function () {
             document.querySelector('.search').classList.toggle('search_open')
         });
     });
+
+    // Открытие меню
     document.querySelectorAll('.nav-opener').forEach(function(item) {
         item.addEventListener('click', function () {
             document.querySelector('.nav').classList.toggle('nav_open')
+        });
+    });
+
+    // Открытие карточек галереи
+    document.querySelectorAll('.galaru__clouse').forEach(function(item) {
+        item.addEventListener('click', function () {
+            document.querySelectorAll('.galary__card').forEach(function(item) {
+                item.classList.remove('galary__card_open')
+            })
+        });
+    });
+    document.querySelectorAll('.galary__slide').forEach(function(item) {
+        item.addEventListener('click', function () {
+            cardID = 'galary-card-' + item.id.slice(-1)
+            document.getElementById(cardID).classList.add('galary__card_open')
         });
     });
   });
