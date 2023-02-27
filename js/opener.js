d75fd85fc2c5640ed23c18ca15d0ad190d7ce0be
Alexandr Rublev <alexandrublev@yandex.ru>
2023-02-27 23:39:02 +0300

    catalog added

--- js/opener.js
+++ js/opener.js
@@ -23,9 +23,40 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     document.querySelectorAll('.galary__slide').forEach(function(item) {
         item.addEventListener('click', function () {
-            cardID = 'galary-card-' + item.id.slice(-1)
+            var cardID = 'galary-card-' + item.id.slice(-1)
             document.getElementById(cardID).classList.add('galary__card_open')
         });
     });
+
+    // Аккардион
+    document.querySelectorAll('.period__yers').forEach(function(item) {
+        item.addEventListener('click', function () {
+            if (item.parentElement.classList.contains('period__item_open')) {
+                item.parentElement.classList.remove('period__item_open')    
+            } else {
+                document.querySelectorAll('.period__item').forEach(function(item) {
+                    item.classList.remove('period__item_open')
+                })
+                item.parentElement.classList.add('period__item_open')
+            }
+        });
+    });
+
+    document.querySelectorAll('.person-btn').forEach(function(item) {
+        item.addEventListener('click', function () {
+            document.querySelectorAll('.bio__card').forEach(function(item) {
+                item.classList.remove('bio__card_open')
+            })
+            var bioID = item.id.slice(-1);
+            if (bioID) {
+                bioID = 'bio-card-' + bioID
+                document.getElementById(bioID).classList.add('bio__card_open')
+            } else {
+                document.getElementById('bio-card-none').classList.add('bio__card_open')
+            }
+        });
+    });
+
+
   });
   
