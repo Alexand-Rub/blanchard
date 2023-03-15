40f0841d7fdfce267b9f0823090fec5264b15e12
Alexandr Rublev <alexandrublev@yandex.ru>
2023-03-15 21:04:01 +0300

    table (786px) version added

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     var swiper = new Swiper(".galary__swiper", {
+        spaceBetween: 38,
         pagination: {
           el: ".swiper-pagination",
           type: "fraction",
@@ -9,21 +10,44 @@ document.addEventListener("DOMContentLoaded", () => {
           prevEl: ".swiper-button-prev",
         },
         loop: true,
+
+        breakpoints: {
+          // when window width is >= 320px
+          768: {
+            slidesPerGroup: 2,
+            slidesPerView: 2,
+          },
+        }
     });
 
     var swiper = new Swiper(".event__swiper", {
+      spaceBetween: 32,
       pagination: {
         el: ".swiper-pagination",
       },
+      breakpoints: {
+        // when window width is >= 320px
+        768: {
+          slidesPerGroup: 2,
+          slidesPerView: 2,
+        },
+      }
     });
 
     var swiper = new Swiper(".projects__swiper", {
-      spaceBetween: 50,
+      spaceBetween: 34,
+      loop: true,
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
-      loop: true,
+      breakpoints: {
+        // when window width is >= 320px
+        768: {
+          slidesPerGroup: 2,
+          slidesPerView: 2,
+        },
+      }
     });
 })
 
